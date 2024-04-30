56d801ed18573c183d8af8b9504227c3fb95e384
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2024-04-30 20:14:12 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python3
-"""Log stats"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
+def get_total_logs(collection):
+    """Returns the total number of logs"""
+    return collection.count_documents({})
 
-def print_logs(logs):
-    """
-    Print logs
-    """
-    print(f"{logs.count()} logs")
-    print("Methods:")
-    print(f"    method GET: {logs.count_documents({'method': 'GET'})}")
-    print(f"    method POST: {logs.count_documents({'method': 'POST'})}")
-    print(f"    method PUT: {logs.count_documents({'method': 'PUT'})}")
-    print(f"    method PATCH: {logs.count_documents({'method': 'PATCH'})}")
-    print(f"    method DELETE: {logs.count_documents({'method': 'DELETE'})}")
-    print(f"{logs.count_documents({'path': '/status'})} status check")
-    print("IPs:")
-    ip_counts = logs.aggregate([
+def get_method_counts(collection, methods):
+    """Returns the count of each HTTP method"""
+    method_counts = {}
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        method_counts[method] = count
+    return method_counts
+
+def get_status_check_count(collection):
+    """Returns the count of status checks"""
+    return collection.count_documents({"method": "GET", "path": "/status"})
+
+def get_top_ips(collection, limit=10):
+    """Returns the top IPs along with their counts"""
+    top_ips = collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
-        {"$limit": 10}
+        {"$limit": limit}
     ])
-    for ip_count in ip_counts:
-        print(f"    {ip_count['_id']}: {ip_count['count']}")
+    return [(ip['_id'], ip['count']) for ip in top_ips]
 
+def print_logs_stats(collection):
+    """Prints statistics about Nginx logs"""
+    print(f"{get_total_logs(collection)} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    method_counts = get_method_counts(collection, methods)
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
+
+    status_check_count = get_status_check_count(collection)
+    print(f"{status_check_count} status check")
+
+    print("IPs:")
+    top_ips = get_top_ips(collection)
+    for ip, count in top_ips:
+        print(f"\t{ip}: {count}")
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    print_logs(logs)
+    nginx_collection = client.logs.nginx
+    print_logs_stats(nginx_collection)
     client.close()
